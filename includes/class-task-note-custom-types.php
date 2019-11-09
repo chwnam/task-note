9daa51f7d4f70c83f0d7398a12519b14cac956f4
changwoo <ep6tri@hotmail.com>
2019-11-09 12:50:13 +0900

    수정 내용 - 택소노미 'project_tag' 추가. - 포스트 타입을 프론트로 공개하되, 관리자 외에는 볼 수 없도록 수정함. - 편집 화면의 제목 placeholder 텍스트 수정.

--- includes/class-task-note-custom-types.php
+++ includes/class-task-note-custom-types.php
@@ -1,27 +1,34 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
 class Task_Note_Custom_Types {
 	public function __construct() {
-		add_action( 'init', [ $this, 'register_post_type' ] );
+		add_action( 'init', [ $this, 'init_callback' ] );
 
 		register_activation_hook( TASK_NOTE_MAIN, [ $this, 'activation_callback' ] );
 		register_deactivation_hook( TASK_NOTE_MAIN, [ $this, 'deactivation_callback' ] );
+
+		if ( ! is_admin() ) {
+			add_filter( 'posts_pre_query', [ $this, 'restrict_to_frontend' ], 10, 2 );
+		} else {
+			add_filter( 'enter_title_here', [ $this, 'modify_editor_title' ], 10, 2 );
+		}
 	}
 
-	public function activation_callback() {
+	public function activation_callback(): void {
+		$this->init_callback();
+		flush_rewrite_rules();
+	}
+
+	public function deactivation_callback(): void {
+		flush_rewrite_rules();
+	}
+
+	public function init_callback(): void {
 		$this->register_post_type();
-		flush_rewrite_rules();
+		$this->register_taxonomy();
 	}
 
-	public function deactivation_callback() {
-		flush_rewrite_rules();
-	}
-
-	public function register_post_type() {
+	public function register_post_type(): void {
 		register_post_type(
 			'task_note',
 			[
@@ -54,18 +61,14 @@ class Task_Note_Custom_Types {
 					'item_updated'             => '업무일지 업데이트됨.',
 				],
 				'description'         => '업무일지 포스트 타입',
-				'public'              => false,
+				'public'              => true,
 				'exclude_from_search' => true,
-				'publicly_queryable'  => false,
-				'show_ui'             => true,
-				'show_in_nav_menus'   => false,
-				'show_in_menu'        => true,
-				'show_in_admin_bar'   => true,
 				'menu_position'       => 5,
 				'menu_icon'           => 'dashicons-text-page',
 				'hierarchical'        => false,
 				'supports'            => [ 'title', 'editor' ],
-				'has_archive'         => false,
+				'taxonomies'          => [ 'project_tag' ],
+				'has_archive'         => true,
 				'rewrite'             => [
 					'slug'       => 'task-note',
 					'with_front' => true,
@@ -73,10 +76,75 @@ class Task_Note_Custom_Types {
 					'pages'      => false,
 					'ep_mask'    => EP_PERMALINK,
 				],
+				'query_var'           => 'task-note',
 				'can_export'          => true,
 				'delete_with_user'    => false,
 				'show_in_rest'        => true,
 			]
 		);
+	}
+
+	public function register_taxonomy(): void {
+		register_taxonomy(
+			'project_tag',
+			[ 'task_note' ],
+			[
+				'labels'             => [
+					'name'                       => '프로젝트 태그',
+					'singular_name'              => '프로젝트 태그',
+					'menu_name'                  => '프로젝트 태그',
+					'all_items'                  => '모든 프로젝트',
+					'edit_item'                  => '프로젝트 수정',
+					'view_item'                  => '프로젝트 보기',
+					'update_item'                => '프로젝트 갱신',
+					'add_new_item'               => '새 프로젝트 추가',
+					'new_item_name'              => '새 프로젝트 이름',
+					'search_items'               => '프로젝트 검색',
+					'popular_items'              => '자주 태그된 프로젝트들',
+					'separate_items_with_commas' => '쉼표로 프로젝트를 구분',
+					'add_or_remove_items'        => '프로젝트 추가 혹은 삭제',
+					'choose_from_most_used'      => '가장 많이 사용된 프로젝트에서 선택',
+					'not_found'                  => '찾을 수 없음',
+					'back_to_items'              => '목록으로 돌하가기',
+				],
+				'public'             => true,
+				'publicly_queryable' => true,
+				'show_admin_column'  => true,
+				'show_ui'            => true,
+				'show_in_menu'       => true,
+				'show_in_nav_menus'  => false,
+				'meta_box_cb'        => null,
+				'show_in_rest'       => true,
+				'description'        => '노트에 프로젝트 태그를 붙여 그 날 어떤 프로젝트를 진행했는지 기록합니다.',
+				'hierarchical'       => false,
+				'query_var'          => 'project-tag',
+				'rewrite'            => [
+					'slug'         => 'project-tag',
+					'with_front'   => true,
+					'hierarchical' => false,
+					'ep_mask'      => EP_NONE,
+				],
+			]
+		);
+	}
+
+	public function restrict_to_frontend( ?array $posts, WP_Query $query ): ?array {
+		if ( $query->is_main_query() ) {
+			remove_filter( 'posts_pre_query', [ $this, 'restrict_to_frontend' ] );
+			if ( ! current_user_can( 'administrator' ) ) {
+				$posts = [];
+			}
+		}
+
+		return $posts;
+	}
+
+	public function modify_editor_title( string $title, WP_Post $post ): string {
+		if ( 'task_note' === $post->post_type ) {
+			$day   = date_i18n( 'D', time() + ( get_option( 'gmt_offset' ) * HOUR_IN_SECONDS ) );
+			$title = current_time( "Y년 m월 d일 ({$day}) 업무일지" );
+		}
+
+		return $title;
 	}
 }
