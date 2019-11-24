2ee6b9bec981cd528547c1e8a089806a69293973
changwoo <ep6tri@hotmail.com>
2019-11-24 12:54:59 +0900

    속성 스크립트에 대한 이벤트 핸들러 추가
    중요도가 높다고 판단하지 않아서 구현하지는 않고
    TODO 주석으로만 남겨 둠.

--- assets/js/time-track-properties.js
+++ assets/js/time-track-properties.js
@@ -1,3 +1,18 @@
 /* global jQuery */
 (function ($) {
+    $('#time_track_date_year, #time_track_date_month, #time_track_date_day').on('change', function () {
+        // TODO: 날짜가 바뀌면 요일 변경
+    });
+
+    $('#time_track_date_year, #time_track_date_month, #time_track_date_day, #time_track_begin_hour, #tn_time_track_begin_minute, #time_track_end_hour, #time_track_end_minute').on('change', function () {
+        // TODO: 시작, 종료 시간이 변경되면 작업 시간 변경
+    });
+
+    $('#timespan').on('change', function () {
+        // TODO: 작업 시간의 변경시 종료 시간 변경
+    });
+
+    $('form#post').on('submit', function () {
+        // TODO: 폼 요소의 정확성 확인 후 폽 제출
+    });
 })(jQuery);
