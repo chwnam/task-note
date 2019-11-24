8b0095ccbc9daded854aa6617b6c1e960e12fa5f
changwoo <ep6tri@hotmail.com>
2019-11-24 12:56:01 +0900

    보조 함수 추가

--- includes/functions.php
+++ includes/functions.php
@@ -15,5 +15,36 @@ function tn_template( string $template_name, array $context = [], bool $echo = t
 			return ob_get_clean();
 		}
 	}
+
 	return null;
+}
+
+
+function format_datetime( int $timestamp, string $format = null ): string {
+	if ( ! $format ) {
+		$format = 'Y-m-d H:i:s';
+	}
+
+	// note: 5.3 부터 가능
+	$date = wp_date( $format, $timestamp );
+
+	if ( ! is_string( $date ) ) {
+		$date = '';
+	}
+
+	return $date;
+}
+
+
+function format_timespan( int $timespan ): string {
+	if ( $timespan > HOUR_IN_SECONDS ) {
+		$hour     = intval( $timespan / HOUR_IN_SECONDS );
+		$timespan %= HOUR_IN_SECONDS;
+	} else {
+		$hour = 0;
+	}
+
+	$minute = intval( $timespan / MINUTE_IN_SECONDS );
+
+	return sprintf( '%02d:%02d', $hour, $minute );
 }
